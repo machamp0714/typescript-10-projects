4e3b52449458f8ccd9ca8c9ca813e2f77f7bf2de
machamp0714 <rateliasudaisuki@gmail.com>
2021-08-09 12:52:54 +0900

    modify: setInterval

--- countdown-timer/countdown-timer.ts
+++ countdown-timer/countdown-timer.ts
@@ -1,19 +1,28 @@
 // CountDown
 
 const newYears = new Date('2022-01-01');
-const currentDate = new Date();
 
-const diffDates = Math.floor((newYears.getTime() - currentDate.getTime()) / (24 * 60 * 60 * 1000));
-const diffHours = 24 - currentDate.getHours();
-const diffMinutes = 60 - currentDate.getMinutes();
-const diffSeconds = 60 - currentDate.getSeconds();
+const setCountdown = () => {
+  const currentDate = new Date();
 
-const dates = document.getElementById('days');
-const hours = document.getElementById('hours');
-const mins = document.getElementById('mins');
-const seconds = document.getElementById('seconds');
+  const diffDates = Math.floor((newYears.getTime() - currentDate.getTime()) / (24 * 60 * 60 * 1000));
+  const diffHours = 24 - currentDate.getHours();
+  const diffMinutes = 60 - currentDate.getMinutes();
+  const diffSeconds = 60 - currentDate.getSeconds();
 
-if (dates) dates.innerText = diffDates.toString();
-if (hours) hours.innerText = diffHours.toString();
-if (mins) mins.innerText = diffMinutes.toString();
-if (seconds) seconds.innerText = diffSeconds.toString();
+  const dates = document.getElementById('days');
+  const hours = document.getElementById('hours');
+  const mins = document.getElementById('mins');
+  const seconds = document.getElementById('seconds');
+
+  if (dates) dates.innerText = diffDates.toString();
+  if (hours) hours.innerText = diffHours.toString();
+  if (mins) mins.innerText = diffMinutes.toString();
+  if (seconds) seconds.innerText = diffSeconds.toString();
+}
+
+const countdown = () => {
+  setInterval(setCountdown, 1000);
+}
+
+countdown();
